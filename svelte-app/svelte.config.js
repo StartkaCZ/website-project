fea5aa8a2ec16a0423e0b9eb8890644f95633f71
StartkaCZ <liborzachoval@gmail.com>
2025-04-01 09:49:51 +0100

    Added automatic deployment

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -1,9 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+const repoName = 'website-project'; // Replace with your actual repository name
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : `/${repoName}`
+		  },
+		  prerender: {
+			default: true
+		  }
 	}
 };
 
